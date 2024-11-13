a7fdda690e7aea68e2c5384218f8aaf4b5e802c7
Jari Honkanen <jaarhonka@yahoo.com>
2024-11-13 09:19:00 +0200

    create script that builds zynq with clock source for fpga

--- tcl/build_all.tcl
+++ tcl/build_all.tcl
@@ -3,19 +3,18 @@ variable tcl_path [ file dirname [ file normalize [ info script ] ] ]
 variable build_path ./
 
 # Step 1: Create a new project
-create_project my_project ./my_project -part xc7z020clg400-1
+create_project my_project ./my_project -part xc7z020clg400-1 -force
 
 set_property target_language VHDL [current_project]
 
-proc program_ram {} {
+proc program_ram {bitfile} {
     open_hw_manager
     connect_hw_server -allow_non_jtag
     open_hw_target
-    set_property PROGRAM.FILE {D:/dev/zynq_scripted_build/build/my_project/my_project.runs/impl_1/zynq_bd_wrapper.bit} [get_hw_devices xc7z020_1]
+    set_property PROGRAM.FILE {$bitfile} [get_hw_devices xc7z020_1]
     current_hw_device [get_hw_devices xc7z020_1]
     set_property PROBES.FILE {} [get_hw_devices xc7z020_1]
     set_property FULL_PROBES.FILE {} [get_hw_devices xc7z020_1]
-    set_property PROGRAM.FILE {D:/dev/zynq_scripted_build/build/my_project/my_project.runs/impl_1/zynq_bd_wrapper.bit} [get_hw_devices xc7z020_1]
     program_hw_devices [get_hw_devices xc7z020_1]
     refresh_hw_device [lindex [get_hw_devices xc7z020_1] 0]
     refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xc7z020_1] 0]
@@ -23,19 +22,50 @@ proc program_ram {} {
 
 
 # Create and configure the block design
-source 
+create_bd_design "zynq_bd"
 
-# here starts the vhdl design
-add_files -norecurse d:/dev/zynq_scripted_build/vhdl_sources/top.vhd
+startgroup
+create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0
+endgroup
 
+set_property -dict [list \
+  CONFIG.PCW_FCLK0_PERIPHERAL_CLKSRC {ARM PLL} \
+  CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ {166.666} \
+  CONFIG.PCW_USE_M_AXI_GP0 {0} \
+] [get_bd_cells processing_system7_0]
+
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" Master "Disable" Slave "Disable" }  [get_bd_cells processing_system7_0]
+startgroup
+    make_bd_pins_external  [get_bd_pins processing_system7_0/FCLK_CLK0]
+    make_bd_pins_external  [get_bd_pins processing_system7_0/FCLK_RESET0_N]
+endgroup
+    
+assign_bd_address
+regenerate_bd_layout
+#
+save_bd_design
+
+generate_target all [get_files  $build_path/my_project/my_project.srcs/sources_1/bd/zynq_bd/zynq_bd.bd]
+
+export_ip_user_files -of_objects [get_files $build_path/my_project/my_project.srcs/sources_1/bd/zynq_bd/zynq_bd.bd] -no_script -sync -force -quiet
+create_ip_run [get_files -of_objects [get_fileset sources_1] $build_path/my_project/my_project.srcs/sources_1/bd/zynq_bd/zynq_bd.bd]
 launch_runs zynq_bd_processing_system7_0_0_synth_1 -jobs 32
+export_simulation -of_objects [get_files $build_path/my_project/my_project.srcs/sources_1/bd/zynq_bd/zynq_bd.bd] -directory $build_path/my_project/my_project.ip_user_files/sim_scripts -ip_user_files_dir $build_path/my_project/my_project.ip_user_files -ipstatic_source_dir $build_path/my_project/my_project.ip_user_files/ipstatic -lib_map_path [list {modelsim=$build_path/my_project/my_project.cache/compile_simlib/modelsim} {questa=$build_path/my_project/my_project.cache/compile_simlib/questa} {riviera=$build_path/my_project/my_project.cache/compile_simlib/riviera} {activehdl=$build_path/my_project/my_project.cache/compile_simlib/activehdl}] -use_ip_compiled_libs -force -quiet
+wait_on_run zynq_bd_processing_system7_0_0_synth_1
 
-write_hw_platform -fixed -force -file $hc_project_path/zynq_bd_wrapper.xsa
+add_files -norecurse $tcl_path/../vhdl_sources/generated_top.vhd
+add_files -norecurse $tcl_path/../vhdl_sources/top.vhd
+
+write_hw_platform -fixed -force -file $build_path/zynq_hw_export.xsa
 
 launch_runs synth_1 -jobs 32
 wait_on_run synth_1
+open_run synth_1 -name synth_1
 set_property IOSTANDARD LVCMOS33 [get_ports [list led_blinker]]
-place_ports led_blinker P15
-launch_runs impl_1 -to_step write_bitstream -jobs 32
+place_ports led_blinker T12
+
+launch_runs impl_1 -jobs 32
 wait_on_run impl_1
 
+launch_runs impl_1 -to_step write_bitstream -jobs 32
+wait_on_run impl_1
--- vhdl_sources/generated_top.vhd
+++ vhdl_sources/generated_top.vhd
@@ -2,7 +2,7 @@
 --Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
---Date        : Sun Oct  6 22:46:36 2024
+--Date        : Tue Nov 12 20:49:53 2024
 --Host        : Tehoin-PC running 64-bit major release  (build 9200)
 --Command     : generate_target zynq_bd_wrapper.bd
 --Design      : zynq_bd_wrapper
@@ -11,7 +11,10 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity top is
+-- library UNISIM;
+-- use UNISIM.VCOMPONENTS.ALL;
+
+entity zynq_bd_wrapper is
   port (
     DDR_addr          : inout STD_LOGIC_VECTOR ( 14 downto 0 );
     DDR_ba            : inout STD_LOGIC_VECTOR ( 2 downto 0 );
@@ -28,48 +31,47 @@ entity top is
     DDR_ras_n         : inout STD_LOGIC;
     DDR_reset_n       : inout STD_LOGIC;
     DDR_we_n          : inout STD_LOGIC;
+    FCLK_CLK0_0       : out STD_LOGIC;
+    FCLK_RESET0_N_0   : out STD_LOGIC;
     FIXED_IO_ddr_vrn  : inout STD_LOGIC;
     FIXED_IO_ddr_vrp  : inout STD_LOGIC;
     FIXED_IO_mio      : inout STD_LOGIC_VECTOR ( 53 downto 0 );
     FIXED_IO_ps_clk   : inout STD_LOGIC;
     FIXED_IO_ps_porb  : inout STD_LOGIC;
-    FIXED_IO_ps_srstb : inout STD_LOGIC;
-    -- led blinker
-    led_blinker : out STD_LOGIC
+    FIXED_IO_ps_srstb : inout STD_LOGIC
   );
-end top;
+end zynq_bd_wrapper;
 
-architecture STRUCTURE of top is
-
-
-  signal clock_100MHz : STD_LOGIC;
-  signal reset_n      : STD_LOGIC;
-
-  signal counter : natural range 0 to 50e6 := 0;
-  signal led_state : STD_LOGIC := '0';
-
+architecture STRUCTURE of zynq_bd_wrapper is
+  component zynq_bd is
+  port (
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC;
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    FCLK_CLK0_0 : out STD_LOGIC;
+    FCLK_RESET0_N_0 : out STD_LOGIC
+  );
+  end component zynq_bd;
 begin
-
-    led_blinker <= led_state;
-
-    process(clock_100MHz)
-    begin
-        if rising_edge(clock_100MHz) then
-            if counter < 50e6 then
-                counter <= counter + 1;
-            else
-                counter <= 0;
-            end if;
-
-            if counter = 0 then
-                led_state <= not led_state;
-            end if;
-
-        end if;
-    end process;
-
-    
-    u_zynq_bd_wrapper: entity work.zynq_bd_wrapper
+zynq_bd_i: component zynq_bd
      port map (
       DDR_addr(14 downto 0)     => DDR_addr(14 downto 0),
       DDR_ba(2 downto 0)        => DDR_ba(2 downto 0),
@@ -86,8 +88,8 @@ begin
       DDR_ras_n                 => DDR_ras_n,
       DDR_reset_n               => DDR_reset_n,
       DDR_we_n                  => DDR_we_n,
-      FCLK_CLK0_0               => clock_100MHz,
-      FCLK_RESET0_N_0           => reset_n,
+      FCLK_CLK0_0               => FCLK_CLK0_0,
+      FCLK_RESET0_N_0           => FCLK_RESET0_N_0,
       FIXED_IO_ddr_vrn          => FIXED_IO_ddr_vrn,
       FIXED_IO_ddr_vrp          => FIXED_IO_ddr_vrp,
       FIXED_IO_mio(53 downto 0) => FIXED_IO_mio(53 downto 0),
